55d5cc6b2ece8aa3e9c86fffdebfe6256e1d9dba
nextred <redouan.elmossaoui@gmail.com>
2019-05-13 17:00:13 +0000

    Init project: add axios && bootstrap libraries to frontend.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+
 import './registerServiceWorker'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
