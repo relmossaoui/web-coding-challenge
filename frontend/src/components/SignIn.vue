fd4885f05c78022b92f3909ec49938552ff3221d
nextred <redouan.elmossaoui@gmail.com>
2019-05-13 23:39:28 +0000

    Add signIn feature: implement signIn using Vuex (frontend).

---
+++ frontend/src/components/SignIn.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="col-md-4 offset-4">
+        <div class="alert alert-danger" role="alert" v-if="errorMessage">
+            {{ errorMessage}}
+        </div>
+        <form>
+            <div class="form-group">
+                <input type="email" class="form-control" id="emailId" aria-describedby="emailHelp" v-model="email" placeholder="Enter email" />
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" id="passwordId" placeholder="Password" v-model="password" />
+            </div>
+
+            <button class="btn btn-primary" @click.prevent="signIn">Submit</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                email: '',
+                password: '',
+            }
+        },
+
+        methods: {
+            async signIn () {
+                this.$store.dispatch('login', {"vm": this, "credentials" :{
+                    "email" : this.email,
+                    "password": this.password
+                }})
+            }
+        },
+
+        computed: {
+            errorMessage() {
+                return this.$store.state.loginErrorMessage;
+            }
+        }
+    }
+</script>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,16 +1,54 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import {API_ENDPOINT_BASE} from './config/index.js';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    isLogged : !!localStorage.getItem('token'),
+    pending : false,
+    loginErrorMessage: ''
   },
-  mutations: {
 
+  mutations: {
+    LOGIN (state) {
+      state.pending = true;
+      state.isLogged = false
+    },
+    LOGIN_SUCCESS(state, token) {
+      state.pending = false;
+      state.isLogged = true;
+      localStorage.setItem('token', token)
+      state.loginErrorMessage = ''
+    },
+    LOGIN_FAILED(state, message) {
+      state.pending = false;
+      state.isLogged = false;
+      state.loginErrorMessage = message;
+    }
   },
   actions: {
+    async login ({commit}, options) {
+      commit('LOGIN');
+      try {
+          let response = await options.vm.$http.post(`${API_ENDPOINT_BASE}signin`,
+              options.credentials,
+              {
+                headers : {
+                  'Content-type': 'application/json',
+                  'X-Requested-With': 'XMLHttpRequest'
+                }
+              }
+          );
 
+          commit('LOGIN_SUCCESS', response.data.token)
+
+          //TO DO : redirect to main shops page
+
+      } catch (error) {
+        commit('LOGIN_FAILED', error.response.data.message)
+      }
+    },
   }
 })
