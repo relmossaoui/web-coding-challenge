b0b675984eca3335737a1450550593ef8db96931
nextred <redouan.elmossaoui@gmail.com>
2019-05-14 14:27:59 +0000

    Shops management feature: implement like shop on frontend.

--- frontend/src/components/Shops.vue
+++ frontend/src/components/Shops.vue
@@ -4,7 +4,7 @@
             <h4> {{ shop.name }} </h4>
             <img class="card-img-top shop-image" :src="shop.image_url" alt="Shop image">
             <div class="card-body shop-links">
-                <a href="#" class="btn btn-success btn-sm">Like</a>
+                <a href="#" class="btn btn-success btn-sm"  @click.prevent="likeShop(shop)">Like</a>
                 <a href="#" class="btn btn-danger">Dislike</a>
             </div>
         </div>
@@ -17,7 +17,8 @@
     export default {
         data () {
             return {
-                shops: []
+                shops: [],
+                errorMessage: null
             }
         },
         async created() {
@@ -36,6 +37,29 @@
             } catch (error) {
                 if (error.response.data.type == 1) {
                     this.$store.dispatch('logout', {"vm": this})
+                }
+            }
+        },
+
+        methods: {
+            async likeShop(shop) {
+                try {
+                    let response = await this.$http.post(`${API_ENDPOINT_BASE}shops/like?token=${localStorage.getItem('token')}`,
+                        {
+                            "shopId" : shop.id
+                        },
+                        {
+                            headers : {
+                                'Content-type': 'application/json',
+                                'X-Requested-With': 'XMLHttpRequest'
+                            }
+                        }
+                    );
+
+                    this.shops.splice(this.shops.indexOf(shop), 1)
+
+                } catch (error) {
+                    this.errorMessage = error.response.data.message
                 }
             }
         }
